207c7d121a8bbe27964e83e18f93f313cba17a83
ilmarvor <ilmarvor@gmail.com>
2014-01-26 00:01:28 +0100

    Переработаны активити 2 и 3. Добавлен обработчик событий для каждой из кнопок во всех 3-х активити. Активити 2 переключается на активити 3, активити 3 переключается на активити 2.

--- app/src/main/java/com/wildcards/app/Activity3.java
+++ app/src/main/java/com/wildcards/app/Activity3.java
@@ -1,17 +1,10 @@
 package com.wildcards.app;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.Button;
 
 public class Activity3 extends Activity {
--- app/src/main/java/com/wildcards/app/MainActivity.java
+++ app/src/main/java/com/wildcards/app/MainActivity.java
@@ -1,16 +1,9 @@
 package com.wildcards.app;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
