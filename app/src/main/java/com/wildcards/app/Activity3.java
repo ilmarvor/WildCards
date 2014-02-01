7092779f2953db0b8ba24c4cb29769e3585da3cf
ilmarvor <ilmarvor@gmail.com>
2014-02-02 00:24:21 +0100

    Добавлен обработчик события на кнопку "Назад". Теперь по нажатию на эту кнопку программа выходит в MainActivity.

--- app/src/main/java/com/wildcards/app/Activity3.java
+++ app/src/main/java/com/wildcards/app/Activity3.java
@@ -3,6 +3,7 @@ package com.wildcards.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -48,6 +49,16 @@ public class Activity3 extends Activity {
             }
         };
         btnDontShowAnymore.setOnClickListener(oclBtnDontShowAnymore);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if (keyCode == event.KEYCODE_BACK){
+            Intent inte = new Intent(this, MainActivity.class);
+            startActivity(inte);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
 }
